1a7d0b09bd32c3bb46535b419e42cb1badf45d4e
kaan <kabuke13@gmail.com>
2024-02-01 23:37:08 +0000

    lab changes

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
     
-    def learn(self):
-        pass
+    def learn(self, lesson_learned):
+        if isinstance(lesson_learned, str):
+            self.knowledge.append(lesson_learned)
